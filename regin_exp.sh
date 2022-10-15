a025e8c54682020b8ca0065811996e87d4003e0e
bywmm <1052135493@qq.com>
2022-10-15 20:52:45 +0800

    new scripts

--- regin_exp.sh
+++ regin_exp.sh
@@ -3,13 +3,13 @@ dataset=$2
 ft=$3
 
 hidden_channels_lst=(32 64)
-n_layers_lst=(3)
+n_layers_lst=(2 3 4)
 
 lr_lst=(0.001 0.005)
 
-weight_decay_lst=(0.0005 0.001 0.005)
+weight_decay_lst=(0. 0.001 0.005)
 
-dropout_lst=(0.2 0.5 0.7)
+dropout_lst=(0.5 0.7)
 
 R_list=(10. 100.)
 
@@ -19,9 +19,9 @@ for n_layer in "${n_layers_lst[@]}"; do
 			for wd in "${weight_decay_lst[@]}"; do
 				for dp in "${dropout_lst[@]}"; do
 					for R in "${R_list[@]}"; do
-							python run_regnn.py --dataset $dataset --model resage --save_postfix $dataset-resage --repeat 10 \
+							python run_regnn.py --dataset $dataset --model regin --save_postfix $dataset-regin --repeat 10 \
 							--device $cuda --num_layers $n_layer --hidden_dim $hidden --dropout $dp \
-							--lr $lr --weight_decay $wd --R $R --feats_type $ft 
+							--lr $lr --weight_decay $wd --R $R --feats_type $ft
 done
 done
 done
--- remixhop_exp.sh
+++ remixhop_exp.sh
@@ -2,14 +2,14 @@ cuda=$1
 dataset=$2
 ft=$3
 
-hidden_channels_lst=(32 64)
-n_layers_lst=(3)
+hidden_channels_lst=(64)
+n_layers_lst=(4)
 
 lr_lst=(0.001 0.005)
 
-weight_decay_lst=(0.0005 0.001 0.005)
+weight_decay_lst=(0. 0.001 0.005)
 
-dropout_lst=(0.2 0.5 0.7)
+dropout_lst=(0.5 0.7)
 
 R_list=(10. 100.)
 
@@ -19,9 +19,9 @@ for n_layer in "${n_layers_lst[@]}"; do
 			for wd in "${weight_decay_lst[@]}"; do
 				for dp in "${dropout_lst[@]}"; do
 					for R in "${R_list[@]}"; do
-							python run_regnn.py --dataset $dataset --model resage --save_postfix $dataset-resage --repeat 10 \
+							python run_regnn.py --dataset $dataset --model remixhop --save_postfix $dataset-remixhop --repeat 10 \
 							--device $cuda --num_layers $n_layer --hidden_dim $hidden --dropout $dp \
-							--lr $lr --weight_decay $wd --R $R --feats_type $ft 
+							--lr $lr --weight_decay $wd --R $R --feats_type $ft
 done
 done
 done
