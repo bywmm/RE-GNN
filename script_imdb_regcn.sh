4dc98c94467d94bc0efcdb84207079f850892e75
bywmm <1052135493@qq.com>
2022-10-15 20:52:20 +0800

    upt res

--- script_imdb_regcn.sh
+++ script_imdb_regcn.sh
@@ -1,1 +1,1 @@
-python run_regnn.py --dataset IMDB --model regcn --save-postfix IMDB-regcn --weight_decay 0.005 --repeat 5 --device $1
+python run_regnn.py --dataset IMDB --model regcn --save_postfix IMDB-regcn --feats_type 1 --weight_decay 0.005 --repeat 10 --device $1
